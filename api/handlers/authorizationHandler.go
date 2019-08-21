e8de61e02ab75b691e73078ddd1ba27f2199467a
kdoebr21 <kodeberic@yahoo.co.jp>
2019-08-21 23:28:56 +0900

    feat: NewRequestに書き換え

--- api/handlers/authorizationHandler.go
+++ api/handlers/authorizationHandler.go
@@ -31,14 +31,22 @@ func GetAccessToken(c echo.Context) error {
 		log.Fatal(err)
 	}
 	requestBuffer := bytes.NewBuffer(requestByte)
-	requestUrl := baseApiUrl + "/access_tokens"
-	res, err := http.Post(requestUrl, "application/json", requestBuffer)
+	req, err := http.NewRequest("POST", baseApiUrl+"/access_tokens", requestBuffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Content-type", "application/json")
+	client := &http.Client{}
+	res, err := client.Do(req)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, "リクエスト失敗")
 	}
+	defer res.Body.Close()
 	if res.StatusCode != http.StatusCreated {
 		return c.JSON(http.StatusInternalServerError, "リクエスト失敗")
 	}
+
+	// レスポンスを受け取る
 	var authResponse models.AuthResponse
 	if err := json.NewDecoder(res.Body).Decode(&authResponse); err != nil {
 		log.Fatal(err)
